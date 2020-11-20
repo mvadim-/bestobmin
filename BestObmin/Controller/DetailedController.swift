991655afd3550f19aa458915adf2a12bcfefbde3
Vadym Maslov <mvadim@me.com>
2020-11-20 13:18:56 +0200

    Add chart for today

--- BestObmin/Controller/DetailedController.swift
+++ BestObmin/Controller/DetailedController.swift
@@ -9,18 +9,97 @@
 import Foundation
 import UIKit
 import RealmSwift
+import Charts
 
-class DetailedController: UIViewController {
+class DetailedController: UIViewController, ChartViewDelegate {
     
     @IBOutlet weak var tv: UITableView!
     @IBOutlet weak var Item: UIBarButtonItem!
+    @IBOutlet weak var chart: UIBarButtonItem!
     var currency:String = ""
     var curList: Results<CurrencyObject>? = nil
+    var chartView: LineChartView = LineChartView()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         Item.title  = currency
         curList     = realmHelper.realmObjectslist(forCurrency: currency)
+    }
+    
+    
+    @IBAction func chart(_ sender: UIBarButtonItem) {
+        tv.isHidden = true
+        let rect = CGRect(
+            origin: tv.frame.origin,
+            size: tv.frame.size
+        )
+        chartView                           = LineChartView.init(frame:rect)
+        chartView.delegate                  = self
+        chartView.chartDescription?.enabled = false
+        chartView.dragEnabled               = true
+        chartView.setScaleEnabled(true)
+        chartView.pinchZoomEnabled          = true
+        
+        // x-axis
+        let llXAxis                 = chartView.xAxis
+        llXAxis.labelPosition       = .bottom
+        llXAxis.gridLineDashLengths = [10, 10]
+        llXAxis.gridLineDashPhase   = 0
+        let leftAxis                = chartView.leftAxis
+        leftAxis.removeAllLimitLines()
+
+        // y-axis
+        let min                                     = curList?.first
+        leftAxis.axisMaximum                        = Double(min!.sell)! + 0.5
+        leftAxis.axisMinimum                        = Double(min!.buy)! - 0.5
+        leftAxis.gridLineDashLengths                = [5, 5]
+        leftAxis.drawLimitLinesBehindDataEnabled    = true
+        
+        chartView.rightAxis.enabled = false
+        chartView.legend.form       = .line
+        chartView.animate(xAxisDuration: 1.5)
+        self.setDataCount(forDateInterval: 1)
+        view.addSubview(chartView)
+    }
+    
+    func setDataCount(forDateInterval date :Int) {
+        
+        var valuesBuy = [ChartDataEntry]()
+        var valuesSell = [ChartDataEntry]()
+
+        curList?.enumerated().forEach({(index, cm :CurrencyObject) in
+            let today = Calendar(identifier: .gregorian).isDateInToday(cm.date)
+            if (today) {
+                valuesBuy.append(ChartDataEntry(x: Double(index), y: Double(cm.buy)!, data: cm.date))
+                valuesSell.append(ChartDataEntry(x: Double(index), y: Double(cm.sell)!, data: cm.date))}
+        })
+        
+        let setBuy                = LineChartDataSet(entries: valuesBuy, label: "Buy")
+        setup(setBuy)
+        setBuy.fillAlpha          = 0.8
+        setBuy.fillColor          = .red
+        
+        let setSell                = LineChartDataSet(entries: valuesSell, label: "Sell")
+        setup(setSell)
+        setSell.fillAlpha          = 0.2
+        setSell.fillColor          = .green
+        
+        let data                = LineChartData(dataSets: [setBuy,setSell])
+        chartView.data          = data
+    }
+    
+    private func setup(_ dataSet: LineChartDataSet) {
+        dataSet.lineDashLengths             = nil
+        dataSet.highlightLineDashLengths    = nil
+        dataSet.setColors(dataSet.label == "Buy" ? .red : .green)
+        dataSet.setCircleColor(dataSet.label == "Buy" ? .red : .green)
+        dataSet.lineWidth                   = 1
+        dataSet.circleRadius                = 3
+        dataSet.drawCircleHoleEnabled       = false
+        dataSet.valueFont                   = .systemFont(ofSize: 9)
+        dataSet.formLineDashLengths         = nil
+        dataSet.formLineWidth               = 1
+        dataSet.formSize                    = 15
     }
     
     override func didReceiveMemoryWarning() {
@@ -49,8 +128,4 @@ extension DetailedController :UITableViewDataSource{
         cell.sell.text  = cm.sell
         return cell
     }
-    
-    
-    
-    
 }
--- BestObmin/realmHelper.swift
+++ BestObmin/realmHelper.swift
@@ -12,9 +12,21 @@ import RealmSwift
 class realmHelper {
     
     class func realmObjectslist(forCurrency currency:String) -> Results<CurrencyObject> {
-        let realm = try! Realm()
-        let predicate = NSPredicate(format: "currency = %@", "\(currency)")
+        let realm       = try! Realm()
+        let predicate   = NSPredicate(format: "currency = %@", "\(currency)")
         return realm.objects(CurrencyObject.self).filter(predicate).sorted(byKeyPath: "date", ascending: false)
+    }
+    
+    class func deleteObjects() {
+        let realm   = try! Realm()
+        let curList = realm.objects(CurrencyObject.self)
+        curList.forEach({(cm :CurrencyObject) in
+            if cm.buy == "" {
+                try! realm.write {
+                    realm.delete(cm)
+                }
+            }
+        })
     }
     
 }
